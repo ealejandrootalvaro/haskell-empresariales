3584472acf63d0d68a285eb845fd5b507432524a
ealejandrootalvaro <ealejandro.otalvaro@gmail.com>
2016-11-10 02:47:29 +0000

    Update Constantes.hs

--- src/Constantes.hs
+++ src/Constantes.hs
@@ -2,7 +2,15 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 module Constantes where
-import Data.ByteString.Char8
+
+import           Network.HaskellNet.IMAP.SSL
+import           Network.HaskellNet.SMTP.SSL as SMTP
+
+import           Network.HaskellNet.Auth (AuthType(LOGIN))
+import qualified Data.ByteString.Char8 as B
+import qualified Data.Text.Lazy as L
+
+
 
 success :: String
 success = "Success"
@@ -10,8 +18,28 @@ success = "Success"
 error' :: String
 error' = "Error"
 
-connectionStr :: ByteString
---connectionStr = "postgres://xlyrtaxdwdozqh:ml6B7YXEWvGLdpcw5ty-BDrcne@ec2-50-19-240-113.compute-1.amazonaws.com:5432/dcku066iig1lq1"
-connectionStr = "postgresql://postgres:94cbd72b4e4133f3417a61adf9a418b1@138.197.15.163:5454/restaurant"
+connectionStr :: B.ByteString
+connectionStr = "postgres://ndvmdfyexshzni:bXvG8_ldqY9bGEMvDK9qY-tZvF@ec2-54-235-207-226.compute-1.amazonaws.com:5432/de2if8v7djdfmi"
+-- Conexion a la base de datos de pago
+--connectionStr = "postgresql://postgres:94cbd72b4e4133f3417a61adf9a418b1@138.197.15.163:5454/restaurant"
 puerto :: Int
 puerto = 8087
+
+
+
+username = "restappranteinfo@gmail.com"
+password = "pekkieressaberlo"
+
+
+
+
+smtpTest recipient newPassword = doSMTPSTARTTLS "smtp.gmail.com" $ \c -> do
+    authSucceed <- SMTP.authenticate LOGIN username password c
+    if authSucceed
+      then sendPlainTextMail recipient username subject body c
+      else print "Authentication error."
+  where subject = "Recuperar contraseña"
+        body    = L.pack("Su nueva contraseña es "++newPassword)
+
+sendMensaje :: String -> String -> IO ()
+sendMensaje recipient newPassword = (smtpTest recipient newPassword) >> return ()
