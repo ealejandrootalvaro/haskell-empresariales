6c8e7d7fb7061335004553a11d0ad7e6b6a150f1
xxzerkerrxx <david.blj95@gmail.com>
2016-11-21 17:42:03 -0500

    Update DbReservations.hs

--- src/Db/DbReservations.hs
+++ src/Db/DbReservations.hs
@@ -15,7 +15,7 @@ import qualified Database.PostgreSQL.Simple as D
 
 
 getReservationsByDate conn rango = do
-    result <- (D.query conn "select id_reservation, user_restaurant,table_restaurant, date_init AT TIME ZONE 'MST', date_end AT TIME ZONE 'MST', amount_people, state from reservation where date_init >= ? and date_init <= ? and state = 4 " ((Reservations.fromInitialDate rango),(Reservations.toFinalDate rango)) :: IO [Reservations.Reservation])
+    result <- (D.query conn "select id_reservation, user_restaurant,table_restaurant, date_init AT TIME ZONE 'CDT', date_end AT TIME ZONE 'CDT', amount_people, state from reservation where date_init >= ? and date_init <= ? and state = 4 " ((Reservations.fromInitialDate rango),(Reservations.toFinalDate rango)) :: IO [Reservations.Reservation])
     return result
     
 insertReservation conn reservacion = do 
